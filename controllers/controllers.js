b221a3c609f558c2f8d5cd38b70ea8148d12b351
marcosocon <oconmarcos97@gmail.com>
2016-04-14 21:00:35 -0300

    added functionality for edit items

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -8,6 +8,8 @@ angular.module('AngularApp.controllers',['firebase'])
     $scope.username = 'Marcos';
     var reference = new Firebase('https://contact-firebase.firebaseio.com/');
     $scope.items = $firebaseArray(reference);
+    $scope.ShowEditForm=false;
+    $scope.ShowAddForm=true;
 
     $scope.addItem = function(){
       console.log("adding data..");
@@ -15,14 +17,45 @@ angular.module('AngularApp.controllers',['firebase'])
         name:$scope.name,
         price:$scope.price,
         photo:$scope.photo
-      });
-      $scope.name = '';
-      $scope.price = '';
-      $scope.photo = '';
+      }).then(function(){
+        clearFields();
+      })
     }
     $scope.removeItem = function(item){
       console.log("removing..");
       $scope.items.$remove(item);
     }
+
+    $scope.showEditForm = function(item){
+      $scope.ShowEditForm=true;
+      $scope.ShowAddForm=false;
+
+      $scope.id = item.$id;
+      $scope.name = item.name;
+      $scope.price = item.price;
+      $scope.photo = item.photo;
+    }
+
+    $scope.editItem = function(){
+      var id = $scope.id;
+
+      var record = $scope.items.$getRecord(id);
+
+      record.name = $scope.name;
+      record.price = $scope.price;
+      record.photo = $scope.photo;
+
+      $scope.items.$save(record).then(function(){
+        console.log(id);
+      })
+
+    }
+
+    function clearFields(){
+      $scope.name = '';
+      $scope.price = '';
+      $scope.photo = '';
+    }
+
   }]);
 
